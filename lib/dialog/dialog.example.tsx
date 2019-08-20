5ec9b0bb85240444f67f8891820728c5c78e013b
许丹琦 <1204253728@qq.com>
2019-08-20 20:01:57 +0800

    dialog增加alert

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,23 +1,31 @@
 import React, { useState } from 'react'
-import Dialog from './dialog'
+import Dialog, { alert } from './dialog'
 
 export default function () {
     const [x, setX] = useState(false)
     return (
         <div>
-            <button onClick={() => setX(!x)}>click</button>
-            <Dialog
-                visible={x}
-                buttons={
-                    [
-                        <button onClick={() => { setX(false) }}>1</button>,
-                        <button>2</button>
-                    ]
-                }
-                onClose={() => { setX(false) }}
-            >
-                <div>content</div>
-            </Dialog>
+            <div>
+                <h1>example1</h1>
+                <button onClick={() => setX(!x)}>click</button>
+                <Dialog
+                    visible={x}
+                    buttons={
+                        [
+                            <button onClick={() => { setX(false) }}>1</button>,
+                            <button>2</button>
+                        ]
+                    }
+                    onClose={() => { setX(false) }}
+                >
+                    <div>content</div>
+                </Dialog>
+            </div>
+            <div>
+                <h1>example2</h1>
+                <button onClick={() => alert("hello")}>click</button>
+            </div>
+
         </div>
     )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement } from "react"
+import ReactDOM from 'react-dom'
 import "./dialog.scss"
 import Icon from "../icon"
 
 interface Props {
     visible: boolean;
-    buttons: Array<ReactElement>,
+    buttons?: Array<ReactElement>,
     onClose: React.MouseEventHandler,
     closeOnClickMask?: boolean;
 }
@@ -19,32 +20,52 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
             props.onClose(e)
         }
     }
-    return (
-        props.visible ?
-            <React.Fragment>
-                <div className="gui-dialog-mask" onClick={onClickMask}></div>
-                <div className="gui-dialog">
-                    <div className="gui-dialog-close" onClick={onClickClose}>
-                        <Icon name="close" />
-                    </div>
-                    <header className="gui-dialog-header">tishi</header>
-                    <main
-                        className="gui-dialog-main"
-                    >{props.children}</main>
-                    <footer className="gui-dialog-footer">
-                        {props.buttons.map((button, index) => {
-                            React.cloneElement(button, { key: index })
-                        })}
-                    </footer>
+
+    const x = props.visible ?
+        <React.Fragment>
+            <div className="gui-dialog-mask" onClick={onClickMask}></div>
+            <div className="gui-dialog">
+                <div className="gui-dialog-close" onClick={onClickClose}>
+                    <Icon name="close" />
                 </div>
-            </React.Fragment>
-            : null
+                <header className="gui-dialog-header">tishi</header>
+                <main
+                    className="gui-dialog-main"
+                >{props.children}</main>
+                <footer className="gui-dialog-footer">
+                    {props.buttons && props.buttons.map((button, index) => {
+                        return React.cloneElement(button, { key: index })
+                    })}
+                </footer>
+            </div>
+        </React.Fragment>
+        : null
+    return (
+        ReactDOM.createPortal(x, document.body)
     )
 }
 
 Dialog.defaultProps = {
     closeOnClickMask: false
 }
+
+const alert = (content: string) => {
+    const component =
+        <Dialog
+            visible={true}
+            onClose={() => {
+                ReactDOM.render(React.cloneElement(component, { visible: false }), div)
+                ReactDOM.unmountComponentAtNode(div)
+                div.remove()
+            }}>
+            {content}
+        </Dialog>
+    const div = document.createElement('div');
+    document.body.append(div);
+    ReactDOM.render(component, div)
+}
+
+export { alert }
 
 
 export default Dialog
