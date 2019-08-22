237852a472a1d91e025370b96c9f40ec04a5d1ff
许丹琦 <1204253728@qq.com>
2019-08-22 16:01:11 +0800

    Layout调试

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,4 +1,4 @@
-import React ,{ReactNodeArray, ReactElement}from 'react';
+import React, { ReactNodeArray, ReactElement } from 'react';
 import "./layout.scss";
 import classes from "../helpers/classes";
 import Aside from "./aside";
@@ -8,19 +8,28 @@ interface Props extends React.HTMLAttributes<HTMLElement> {
 }
 
 const Layout: React.FunctionComponent<Props> = (props) => {
-    const {className,...rest} = props;
-    let hasAside = false;
-    if ((props.children as ReactNodeArray).length){
-        (props.children as ReactNodeArray).map(node => {
-           console.log(node)
-           if ((node as ReactElement).type === Aside) {
-                console.log("true")
-                hasAside = true
-           }
-        })
-    }
+    const { className, ...rest } = props;
+    // let hasAside = false;
+    // if ((props.children as ReactNodeArray).length) {
+    //     (props.children as ReactNodeArray).map(node => {
+    //         console.log(node)
+    //         if ((node as ReactElement).type === Aside) {
+    //             console.log("true")
+    //             hasAside = true
+    //         }
+    //     })
+    // }
+
+    const hasAside =
+        (props.children as ReactNodeArray).length &&
+        (props.children as Array<ReactElement>).reduce(
+            (result, node) => result || node.type === Aside,
+            false
+        )
+
+
     return (
-        <div className={!hasAside ? classes('gui-layout', className ): classes('gui-layout',className,'hasAside')} {...rest}>
+        <div className={!hasAside ? classes('gui-layout', className) : classes('gui-layout', className, 'hasAside')} {...rest}>
             {props.children}
         </div>
     )
