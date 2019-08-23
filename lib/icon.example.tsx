99f6741b4f619bb1fc7c2676eedfa1fb4908e2e0
许丹琦 <1204253728@qq.com>
2019-08-23 16:01:07 +0800

    加入源代码

--- lib/icon.example.tsx
+++ lib/icon.example.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Icon from './icon';
 
+const code = require('!!raw-loader!./icon.example.tsx');
+
+
 
 const fn: React.MouseEventHandler = (e) => {
     console.log(e)
@@ -8,7 +11,12 @@ const fn: React.MouseEventHandler = (e) => {
 
 const IconExample: React.FunctionComponent = () => {
     return (
-        <Icon name="wechat" onClick={fn} />
+        <div>
+            <Icon name="wechat" onClick={fn} />
+            <pre>
+                {code.default}
+            </pre>
+        </div>
     )
 }
 
