8496c748dca0e2c0bc6985aa17804afade004ff7
许丹琦 <1204253728@qq.com>
2019-08-24 11:19:05 +0800

    调整

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,21 @@
 const base = require('./webpack.config.js')
 
+
 module.exports = Object.assign({}, base, {
     mode: 'production',
 
-    externals: {
-        react: {
-            commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React'
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'react-dom',
-            root: 'ReactDOM'
-        }
-    }
+    // externals: {
+    //     react: {
+    //         commonjs: 'react',
+    //         commonjs2: 'react',
+    //         amd: 'react',
+    //         root: 'React'
+    //     },
+    //     'react-dom': {
+    //         commonjs: 'react-dom',
+    //         commonjs2: 'react-dom',
+    //         amd: 'react-dom',
+    //         root: 'ReactDOM'
+    //     }
+    // }
 })
